64afd74655e84728d93f2e51f0ae2ea0d97c18d9
miguelprogrammer <miguel.silva@mackenzista.com.br>
2026-03-19 02:56:53 -0300

    :wrench: ajust cors

--- src/main/java/com/compareproduct/meli/config/SwaggerConfig.java
+++ src/main/java/com/compareproduct/meli/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.compareproduct.meli.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://miguelprogrammer-challenge.duckdns.org").description("Servidor de Produção"))
                 .info(new Info()
                         .title("Product Comparison API")
                         .version("1.0.0")
